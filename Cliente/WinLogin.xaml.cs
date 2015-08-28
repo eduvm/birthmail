b0f870d85e298a2f96401805b162c11aa6bb5088
Eduardo Vieira Mendes <eduardovmendes@gmail.com>
2015-08-28 09:30:46 -0300

    Adicionado encryptação de senha do usuário

--- Cliente/WinLogin.xaml.cs
+++ Cliente/WinLogin.xaml.cs
@@ -2,56 +2,79 @@
 using System.Data;
 using System.Windows;
 
-namespace Cliente {
+namespace Cliente
+{
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window
+    {
 
-        public MainWindow() {
+        public MainWindow()
+        {
             InitializeComponent();
         }
 
-        private void btnFechar_Click(object sender, RoutedEventArgs e) {
+        private void btnFechar_Click(object sender, RoutedEventArgs e)
+        {
 
             this.Close();
 
         }
 
-        private void btnLogar_Click(object sender, RoutedEventArgs e) {
+        private void btnLogar_Click(object sender, RoutedEventArgs e)
+        {
 
 
-            try {
+            try
+            {
 
+                // Gera objeto de encriptação
+                EncryptHelper teste = new EncryptHelper();
+
+                // Encrypta senha
+                var senha = teste.Encrypt(tbPass.Password);
+
+                // PEga informacao de usuario
+                var user = tbUser.Text;
+
+                // Gera novo objeto de conexao ao banco de dados
                 DatabaseHelper usuario = new DatabaseHelper("aniversariantes");
-                
-                DataTable recipe;
 
-                String query = String.Format("SELECT usuario FROM dados.usuarios WHERE usuario = '{0}' and senha = '{1}';",tbUser.Text,tbPass.Password);
+                // Define SQL Query
+                String query = String.Format("SELECT usuario FROM dados.usuarios WHERE usuario = '{0}' and senha = '{1}' and ativo = true;", user, senha);
 
-
+                // Executa a query
                 var result = usuario.ExecuteScalar(query);
 
+                // Verifica se existe o usuario no banco
+                if (String.IsNullOrEmpty(result))
+                {
 
-                if (String.IsNullOrEmpty(result)) {
+                    // Informa que não encontrou o usuário
+                    MessageBox.Show("Usuário ou senha incoretos");
 
-                    MessageBox.Show("Usuário ou senha não encontrados");
-                
                 }
-                else {
 
-                    // TODO - Rotina de acessar formulário principal
+                else
+                {
+
+                    // Carrega o formulário principal
                     WinPrincipal winPrincipal = new WinPrincipal();
+
+                    // Mostra o formulário principal
                     winPrincipal.Show();
 
-                    this.Close();    
-                
+                    // Fecha o formulário de login
+                    this.Close();
+
                 }
 
-
             }
-            catch (Exception fail) {
+
+            catch (Exception fail)
+            {
 
                 String error = "The following error has occurred:\n\n";
 
