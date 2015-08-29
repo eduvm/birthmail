13caa6fdc9c58c866397097a3aea0c4600488667
Eduardo Vieira Mendes <eduardovmendes@gmail.com>
2015-08-29 09:44:43 -0300

    Adicionado função para mover a janela ao clicar no título Formatação de código

--- Cliente/WinPrincipal.xaml.cs
+++ Cliente/WinPrincipal.xaml.cs
@@ -1,24 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Cliente {
+
     /// <summary>
     /// Interaction logic for WinPrincipal.xaml
     /// </summary>
     public partial class WinPrincipal : Window {
+
         public WinPrincipal() {
             InitializeComponent();
         }
+
+        private void titleBar_MouseDown(object sender, MouseButtonEventArgs e) {
+            this.DragMove();
+
+        }
+
+        //private DataTable CarregaRecentes() {
+
+        //    // Gera novo objeto de conexao ao banco de dados
+        //    DatabaseHelper recentes = new DatabaseHelper("aniversariantes");
+
+        //    // Define SQL Query
+        //    String query = String.Format("SELECT * FROM");
+
+        //    // Executa a query
+        //    //var result = usuario.ExecuteScalar(query);
+
+        //}
+
     }
 }
