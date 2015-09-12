728087896979c9706af8dee255e578acea0bed7d
Eduardo Vieira Mendes <eduardovmendes@gmail.com>
2015-09-12 09:26:41 -0300

    Adequações no código

--- Cliente/TextBoxConverter.cs
+++ Cliente/TextBoxConverter.cs
@@ -1,25 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Usings
+
+using System;
+using System.Globalization;
 using System.Windows.Data;
 
+#endregion
+
 namespace Cliente {
-    class TextBoxConverter : IValueConverter {
 
+    /// <summary>
+    ///     Classe que faz conversão de valores ao utilizar o bind
+    /// </summary>
+    internal class TextBoxConverter : IValueConverter {
 
+        /// <summary>
+        ///     Método que vai cuidar do tamanho da área de texto do textblock da janela de login
+        /// </summary>
+        /// <param name="value">Parametro recebido do bind Width do retangulo</param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns>Valor alterado</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            // Seta valor do width com o tamanho recebido (tamanho do retangulo) menos a diferença
+            var val = (int) (double) value - 80;
 
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-
-            int val = (int)(double)value - 80;
-
+            // Retorna o valor corrigido
             return val;
-
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            // Deveria fazer a conversão inversa, mas a mesma não é necessária
             throw new NotImplementedException();
         }
+
     }
+
 }
