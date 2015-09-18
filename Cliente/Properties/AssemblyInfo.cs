e69b3d047cf7523112f13b7c82cc277275f9ec1c
Eduardo Vieira Mendes <eduardovmendes@gmail.com>
2015-09-18 13:15:01 -0300

    Alterado ordem de parada dos componentes ao apertar tab

--- Cliente/Properties/AssemblyInfo.cs
+++ Cliente/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.261.49")]
-[assembly: AssemblyFileVersion("0.1.261.49")]
+[assembly: AssemblyVersion("0.1.261.50")]
+[assembly: AssemblyFileVersion("0.1.261.50")]
